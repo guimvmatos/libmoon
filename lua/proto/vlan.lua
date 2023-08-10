300298f490039706ae1e47e9f864efc3843f83ed
Guilherme Matos <guimvmatos1@outlook.com>
2023-08-10 20:24:12 -0300

    Update vlan.lua

--- lua/proto/vlan.lua
+++ lua/proto/vlan.lua
@@ -54,8 +54,9 @@ vlanHeader.__index = vlanHeader
 --- Set the TCI
 --- @param int TCI of the Vlan header as 16 bit integer.
 function vlanHeader:setTci(int)
-	int = int or 0 --determinar todo
+	int = int or 4095
 	self.tci = hton1t(int)
+end
 
 --- Retrieve the TCI.
 --- @return TCI as 16 bit integer.
@@ -69,7 +70,23 @@ function vlanHeader:getTciString()
 	return self:getTci()
 end
 
-todo, fazer o mesmo esqueme acima, set, get, getstring para o ether_type
+--- Set the Ether_type
+--- @param int Ether_type of the Vlan header as 16 bit integer.
+function vlanHeader:setEtherType(int)
+	int = int or 8100
+	self.ether_type = hton1t(int)
+
+--- Retrieve the Ether_type.
+--- @return Ether_type as 16 bit integer.
+function vlanHeader:getEtherType()
+	return hton16(self.ether_type)
+end
+
+--- Retrieve the Ether_type as string.
+--- @return Ether_type as string.
+function vlanHeader:getEtherTypeString()
+	return self:getEtherType()
+end
 
 --- Set all members of the PROTO header.
 --- Per default, all members are set to default values specified in the respective set function.
@@ -80,30 +97,32 @@ todo, fazer o mesmo esqueme acima, set, get, getstring para o ether_type
 --- fill() -- only default values
 --- fill{ PROTOXYZ=1 } -- all members are set to default values with the exception of PROTOXYZ, ...
 --- @endcode
-function PROTOHeader:fill(args, pre)
+function vlanHeader:fill(args, pre)
 	args = args or {}
-	pre = pre or "PROTO"
+	pre = pre or "vlan"
 
-	self:setXYZ(args[pre .. "PROTOXYZ"])
+	self:setTci(args[pre .. "Tci"])
+	self:setEtherType(args[pre .. "Ether_type"])
 end
 
 --- Retrieve the values of all members.
---- @param pre prefix for namedArgs. Default 'PROTO'.
+--- @param pre prefix for namedArgs. Default 'vlan'.
 --- @return Table of named arguments. For a list of arguments see "See also".
---- @see PROTOHeader:fill
-function PROTOHeader:get(pre)
-	pre = pre or "PROTO"
+--- @see vlanHeader:fill
+function vlanHeader:get(pre)
+	pre = pre or "vlan"
 
 	local args = {}
-	args[pre .. "PROTOXYZ"] = self:getXYZ() 
+	args[pre .. "Tci"] = self:getTci()
+	args[pre .. "Ether_type"] = self:getEtherType() 
 
 	return args
 end
 
 --- Retrieve the values of all members.
 --- @return Values in string format.
-function PROTOHeader:getString()
-	return "PROTO " .. self:getXYZString()
+function vlanHeader:getString()
+	return "VLAN TCI" .. self:getTciString() .. " Vlan EtherType " .. self:getEtherTypeString()
 end
 
 --- Resolve which header comes after this one (in a packet)
@@ -119,13 +138,13 @@ end
 --- This can be used to for instance calculate a length value based on the total packet length
 --- See proto/ip4.setDefaultNamedArgs as an example
 --- This function must exist and is only used by packet.fill
---- @param pre The prefix used for the namedArgs, e.g. 'PROTO'
+--- @param pre The prefix used for the namedArgs, e.g. 'vlan'
 --- @param namedArgs Table of named arguments (see See more)
 --- @param nextHeader The header following after this header in a packet
 --- @param accumulatedLength The so far accumulated length for previous headers in a packet
 --- @return Table of namedArgs
 --- @see PROTOHeader:fill
-function PROTOHeader:setDefaultNamedArgs(pre, namedArgs, nextHeader, accumulatedLength)
+function vlanHeader:setDefaultNamedArgs(pre, namedArgs, nextHeader, accumulatedLength)
 	return namedArgs
 end
 
@@ -134,7 +153,7 @@ end
 ---- Metatypes
 ------------------------------------------------------------------------
 
-PROTO.metatype = PROTOHeader
+vlan.metatype = vlanHeader
 
 
-return PROTO
+return vlan
